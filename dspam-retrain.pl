1c958dd41b349203f6273e9cd1d6c4720df39622
Forth <forth@ophane.net>
2012-10-10 16:00:20 +0200

    initial commit

---
+++ dspam-retrain.pl
@@ -0,0 +1,72 @@
+#!/usr/bin/perl
+use Logger::Syslog;
+
+my $DSPAM = '/usr/local/bin/dspam';
+# Get arguments
+my $class  = shift;
+my $sender = shift;
+my $recip  = shift;
+
+error("dspam-retrain called without class") unless defined $class;
+error("dspam-retrain called without sender") unless defined $sender;
+error("dspam-retrain called without recip") unless defined $recip;
+die("dspam-retrain not properly initiated") unless defined($class) and defined($sender) and defined($recip);
+   
+my $user;
+#if ($recip =~ /^(?:spam|ham)[_-](\w+)@/) {
+    # username is part of the recipient
+#    $user = $1;
+#} elsif ($sender =~ /^(\w+)@/) {
+    # username is in the sender
+#    $user = $1;
+#} else {
+#    print "Can't determine user\n";
+#    notice("dspam-retrain can't determine user");
+#    exit 75;   # EX_TEMPFAIL
+#}
+
+# Pull out DSPAM signatures and send them to the dspam program
+my $sub;
+my $sig;
+while (<>) {
+
+   if ((! $user) && (/^[ >]*Delivered-To:\s*(\S+)/i)) {
+        $user = $1;
+    }
+    elsif ((! $subj) && (/^Subject: /)) {
+        $subj = $_;
+    } elsif (/^[ >]*X-DSPAM-Signature:\s*(\S+)/i) {
+        $sig = $1;
+        last;
+    } elsif ((! $sig) && (/^[ >]*!DSPAM:\s*(S+)/)) {
+        $sig = $1;
+        last;
+}
+}
+
+
+if (! defined($subj)) {
+    error("dspam-retrain can't find messages subject");
+    die("dspam-retrain can't find messages subject");
+}
+if (! defined($sig)) {
+    error("dspam-retrain can't find DSPAM signature");
+    die("dspam-retrain can't find DSPAM signature");
+}
+
+
+my $ret = system($DSPAM, '--client', '--source=error', "--class=$class", '--user', $user, "--signature=$sig");
+if ($ret==0) {
+    notice("$DSPAM retrained with:$class; $user; $sig; $subj");
+    notice("$ret");
+} else {
+    error("dspam-retrain error on: $DSPAM --source=error --class=$class --user $user --signature=$sig; $subj;");
+    if ($ret == -1) {
+        error("dspam-retrain: $DSPAM failed to execute: $!");
+    } elsif ($ret & 127) {
+        error(sprintf "dspam-retrain: child died with signal %d, %s coredump\n", ($? & 127), ($? & 128) ? 'with' :  'without' );
+    }
+    else {
+        error(sprintf "dspam-retrain: child exited with value %d\n", $? >> 8);
+    }
+}
